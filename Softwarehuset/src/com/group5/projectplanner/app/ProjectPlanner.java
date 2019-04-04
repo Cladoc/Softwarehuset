8df5436fe5040e93bd7f03eed504b4bfc4acec18
Anders Hass <s163952@student.dtu.dk>
2019-04-04 17:46:59 +0200

    removal of comments

--- Softwarehuset/src/com/group5/projectplanner/app/ProjectPlanner.java
+++ Softwarehuset/src/com/group5/projectplanner/app/ProjectPlanner.java
@@ -1,11 +1,5 @@
 package com.group5.projectplanner.app;
 
 public class ProjectPlanner {
-	// Hej Casper
-	// Hej Anders
-	// Hej - Marko
-	// Hej igen Marko
-	// Hej Anders. Du er en kegle
-	// Test
-	// Endnu en test
+	
 }
