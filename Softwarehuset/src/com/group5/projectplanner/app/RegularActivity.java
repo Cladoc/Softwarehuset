7d12c89280ef18af23c009413f24604f7b58ccf4
Casper Joergensen <s163950@student.dtu.dk>
2019-04-08 14:02:30 +0200

    Updated activity classes and moved use case implementations

--- Softwarehuset/src/com/group5/projectplanner/app/RegularActivity.java
+++ Softwarehuset/src/com/group5/projectplanner/app/RegularActivity.java
@@ -1,5 +1,15 @@
 package com.group5.projectplanner.app;
 
-public class RegularActivity {
+import com.group5.projectplanner.app.Developer;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class RegularActivity extends Activity {
+	
+	private List<Developer> developerList = new ArrayList<>();
+	private double totalExpectedHours;
+	private boolean complete;
+	
+	
 }
---
+++ Softwarehuset/tests/com/group5/featuretests/developerTests.java
@@ -0,0 +1,59 @@
+package com.group5.featuretests;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class developerTests {
+
+
+	@Given("that a developer is registered in the project planner")
+	public void thatADeveloperIsRegisteredInTheProjectPlanner() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@When("the developer adds a project with the name {string} and start year of {int}")
+	public void theDeveloperAddsAProjectWithTheNameAndStartYearOf(String string, Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@Then("the project with the name {string} and start year of {int} is added to the project planner")
+	public void theProjectWithTheNameAndStartYearOfIsAddedToTheProjectPlanner(String string, Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@When("the developer adds a project with the name {string} and start year of {string}")
+	public void theDeveloperAddsAProjectWithTheNameAndStartYearOf(String string, String string2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@Then("he gets the error message {string}")
+	public void heGetsTheErrorMessage(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@Given("a project is registered with the name {string} When the developer adds a project with the name {string} and start year of {int}")
+	public void aProjectIsRegisteredWithTheNameWhenTheDeveloperAddsAProjectWithTheNameAndStartYearOf(String string, String string2, Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@Given("that a developer is not registered in the project planner")
+	public void thatADeveloperIsNotRegisteredInTheProjectPlanner() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+
+	@Then("he gets an error message of {string}")
+	public void heGetsAnErrorMessageOf(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new cucumber.api.PendingException();
+	}
+	
+	
+}
