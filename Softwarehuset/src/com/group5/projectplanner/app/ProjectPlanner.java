5dbd6960e2d2723a23c1ebc11009239bf82c1e23
AndersHeileHass <andish_hass@hotmail.com>
2019-04-11 13:22:10 +0200

    Fields and methods added to ProjectPlanner

--- Softwarehuset/src/com/group5/projectplanner/app/ProjectPlanner.java
+++ Softwarehuset/src/com/group5/projectplanner/app/ProjectPlanner.java
@@ -1,5 +1,24 @@
 package com.group5.projectplanner.app;
+import java.util.List;
+import java.util.ArrayList;
+
+
+//import Developer
+//import Project
 
 public class ProjectPlanner {
-	
+	List developers; //list of developers
+	List projects; //list of projects
+	public static void addDeveloper() {
+		
+	}
+	public static void addProject() {
+		
+	}
+	public static void checkProjectExist() {
+		
+	}
+	public static void checkDeveloperExist() {
+		
+	}
 }
